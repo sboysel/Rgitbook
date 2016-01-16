2b9577abda5b11bbf69539d316a0391e0f8eb5ba
Sam Boysel <sboysel@gmail.com>
2016-01-15 16:46:49 -0800

    use which instead of an test run

--- R/checkForGitbook.R
+++ R/checkForGitbook.R
@@ -6,10 +6,10 @@
 #' @param quiet logical indicating whether messages should be printed.
 #' @export
 checkForGitbook <- function(quiet=FALSE) {
-	if(system('npm', ignore.stdout=TRUE) != 0) {
+	if(identical(Sys.which("npm"), "")) {
 		stop("Cannot find node.js. You can install it from http://nodejs.org/download/")
 	}
-	if(system('gitbook', ignore.stdout=TRUE) != 0) {
+	if(identical(Sys.which("gitbook"), "")) {
 		if(!quiet) { message("Installing gitbook...") }
 		installGitbook()
 	}
