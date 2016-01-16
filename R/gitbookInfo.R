23b8c08eab2445813dac0533cf09d5e686a3079d
Sam Boysel <sboysel@gmail.com>
2016-01-15 16:49:32 -0800

    use gitbook-cli

--- R/gitbookInfo.R
+++ R/gitbookInfo.R
@@ -8,18 +8,18 @@
 gitbookInfo <- function() {
 	checkForGitbook(quiet=TRUE)
 	installed <- system('gitbook --version', intern=TRUE)
-	current <- system('npm view gitbook version', intern=TRUE)
+	current <- system('npm view gitbook-cli version', intern=TRUE)
 	if(length(current) > 0) {
 		current <- current[1]
 		if(current == installed) {
-			message(paste0('gitbook is up-to-date with version ', current))
+			message(paste0('gitbook-cli is up-to-date with version ', current))
 		} else {
-			message(paste0('A new version of gitbook is available. Version ',
+			message(paste0('A new version of gitbook-cli is available. Version ',
 						   installed, ' installed, ', current, ' available.'))
 		}
 	} else {
-		warning(paste0('Could not get the current available version of gitbook.',
-					   'Are you connected to the interent?'))
+		warning(paste0('Could not get the current available version of ',
+					  'gitbook-cli.  Are you connected to the interent?'))
 	}
 	invisible(c(installed.version=installed, 
 				available.version=current))
