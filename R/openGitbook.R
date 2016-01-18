9d6758f741f004edf831d9f39ef9d6a4a5375e25
Sam Boysel <sboysel@gmail.com>
2016-01-17 20:50:57 -0800

    error if gitbook does not exits

--- R/openGitbook.R
+++ R/openGitbook.R
@@ -3,13 +3,16 @@
 #' This function is a wrapper to the system call of \code{open} which should
 #' open the book in the system's default web browser.
 #'
-#' @param out.dir location of the built gitbook.
+#' @param out.dir location of the built gitbook.  The function will open
+#' "_book/index.html" in a web browser.
 #'
 #' @export
-openGitbook <- function(out.dir = file.path(getwd(), "_book")) {
-	if (!grepl("_book", out.dir)) {
-	  browseURL(file.path(out.dir, "_book", "index.html"))
+openGitbook <- function(out.dir = getwd()) {
+  out.dir <- normalizePath(out.dir)
+	if (!any(grepl("_book", dir(out.dir)))) {
+	  stop(paste("Gitbook not found in", out.dir))
 	} else {
-	  browseURL(file.path(out.dir, "index.html"))
+	  browseURL(file.path(out.dir, "_book", "index.html"))
+	  invisible(TRUE)
 	}
 }
