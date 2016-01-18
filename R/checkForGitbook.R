01707d084d009658db4864860c69f568f1dafee0
Sam Boysel <sboysel@gmail.com>
2016-01-17 17:31:10 -0800

    warn for missing gitbook-cli

--- R/checkForGitbook.R
+++ R/checkForGitbook.R
@@ -1,18 +1,21 @@
 #' Check if Gitbook is installed.
-#' 
+#'
 #' If Gitbook is not installed it will try to do so. If the installion fails or
 #' Node.js is not installed, an error will be thrown.
-#' 
+#'
 #' @param quiet logical indicating whether messages should be printed.
 #' @export
 checkForGitbook <- function(quiet=FALSE) {
-	if(identical(Sys.which("npm"), "")) {
+	if (identical(Sys.which("npm"), "")) {
 		stop("Cannot find node.js. You can install it from http://nodejs.org/download/")
 	}
-	if(identical(Sys.which("gitbook"), "")) {
-		if(!quiet) { message("Installing gitbook...") }
-		installGitbook()
+	if (identical(Sys.which("gitbook"), "")) {
+		if (!quiet) {
+		  message("gitbook-cli not found.  Run 'npm install gitbook-cli -g' as root")
+		}
 	}
-	if(!quiet) { message('gitbook is installed and available')}
+	if (!quiet) {
+	  message('gitbook is installed and available')
+	}
 	invisible(TRUE)
 }
