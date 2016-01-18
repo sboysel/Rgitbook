82d838d58008032d66b56d6ff3471f15d0afe94d
Sam Boysel <sboysel@gmail.com>
2016-01-17 18:22:47 -0800

    cleanup

--- R/buildGitbook.R
+++ R/buildGitbook.R
@@ -1,40 +1,40 @@
 #' This will build a gitbook from the source markdown files.
-#' 
+#'
 #' This function is simply a wrapper to a system call to \code{gitbook}.
-#' 
+#'
 #' \url{https://github.com/GitbookIO/gitbook}
-#' 
+#'
 #' @param source.dir location containing the source files.
 #' @param out.dir location of the built book.
-#' @param format the format of book. Options are pdf or ebook. If omitted, 
+#' @param format the format of book. Options are pdf or ebook. If omitted,
 #'        this will build a website.
 #' @param buildRmd should \code{\link{buildRmd}} be called first.
 #' @param gitbook.params other parameters passed to the gitbook command.
 #' @param ... other parameters passed to \code{\link{buildRmd}}.
 #' @export
-buildGitbook <- function(source.dir=getwd(),
-						 out.dir=paste0(getwd(), '/_book'),
+buildGitbook <- function(source.dir = getwd(),
+						 out.dir = paste0(getwd(), '/_book'),
 						 buildRmd = TRUE,
 						 format,
 						 gitbook.params, ...) {
-	if(buildRmd) {
+	if (buildRmd) {
 		message('Building R markdown files...')
 		buildRmd(source.dir, ...)
 		message('R Markdown files successfully built!')
 	}
-	
+
 	checkForGitbook(quiet=TRUE)
-	
+
 	buildCmd <- 'build'
-	if(!missing(format)) { buildCmd <- format }
+	if (!missing(format)) { buildCmd <- format }
 	cmd <- paste0("gitbook ", buildCmd, " ", source.dir, " --output=", out.dir)
 	#if(!missing(title)) { cmd <- paste0(cmd, ' --title="', title, '"') }
 	#if(!missing(intro)) { cmd <- paste0(cmd, ' --intro="', intro, '"') }
 	#if(!missing(github)) { cmd <- paste0(cmd, ' --github=', github) }
-	#if(mathjax) { cmd <- paste0(cmd, " --plugins plugin-mathjax") }
-	if(!missing(gitbook.params)) { cmd <- paste0(cmd, " ", gitbook.params)}
+	#if (mathjax) { cmd <- paste0(cmd, " --plugins plugin-mathjax") }
+	if (!missing(gitbook.params)) { cmd <- paste0(cmd, " ", gitbook.params)}
 	system(cmd)
-	
+
 	# Post-process hack to fix broken img urls.
 	# https://github.com/GitbookIO/gitbook/issues/99
 	# Will also fix links to the Introduction
@@ -42,7 +42,7 @@ buildGitbook <- function(source.dir=getwd(),
 	#dirs <- list.dirs(out.dir, recursive=FALSE, full.names=FALSE)
 	#for(i in seq_along(dirs)) {
 		files <- list.files(out.dir, '*.html', recursive=TRUE)
-		for(j in seq_along(files)) {
+		for (j in seq_along(files)) {
 			fconn <- file(paste0(out.dir, '/', files[j]))
 			file <- readLines(fconn)
 			close(fconn)
--- R/buildRmd.R
+++ R/buildRmd.R
@@ -1,14 +1,14 @@
 #' Builds markdown files from all Rmarkdown files in the given directories.
-#' 
+#'
 #' This function will build Rmarkdown files in the given directory to markdown.
 #' The default is to traverse all subdirectories of the working directory
 #' looking for .Rmd files to process. This function will save a file in the
 #' working directory called \code{.rmdbuild} that contain the status of the
-#' last successful build. This allows the function to only process changed files. 
-#' 
+#' last successful build. This allows the function to only process changed files.
+#'
 #' @param dir root directory of the gitbook project.
-#' @param clean if TRUE, all Rmd files will be built regardless of their 
-#'        modification date. 
+#' @param clean if TRUE, all Rmd files will be built regardless of their
+#'        modification date.
 #' @param log.dir if specified, the output from \code{\link{knitr}} will be saved
 #'        to a log file in the given directory.
 #' @param log.ext if log files are saved, the file extension to use.
@@ -17,15 +17,15 @@
 buildRmd <- function(dir = getwd(), clean=FALSE, log.dir, log.ext='.txt', ...) {
 	dir <- normalizePath(dir)
 	bib <- NULL
-	
-	if(!exists('statusfile')) {
+
+	if (!exists('statusfile')) {
 		statusfile <- '.rmdbuild'
 		statusfile <- paste0(dir, '/', statusfile)
 	}
-	
+
 	rmds <- list.files(dir[1], '.rmd$', ignore.case=TRUE, recursive=TRUE, full.names=TRUE)
 	finfo <- file.info(rmds)
-	
+
 	referenceFiles <- c()
 
 	# Handle reference files separately. They will be built everytime to ensure
@@ -35,7 +35,7 @@ buildRmd <- function(dir = getwd(), clean=FALSE, log.dir, log.ext='.txt', ...) {
 		referenceFiles <- rmds[referenceFilesPos]
 		rmds <- rmds[-referenceFilesPos]
 	}
-	
+
 	if(!clean & file.exists(statusfile)) {
 		load(statusfile)
 		newfiles <- row.names(finfo)[!row.names(finfo) %in% row.names(rmdinfo)]
@@ -43,12 +43,12 @@ buildRmd <- function(dir = getwd(), clean=FALSE, log.dir, log.ext='.txt', ...) {
 		existing <- existing[finfo[existing,]$mtime > rmdinfo[existing,]$mtime]
 		rmds <- c(newfiles, existing)
 	}
-	
+
 	if(length(referenceFiles) > 0) {
 		# This will ensure the reference files are built last.
 		rmds <- c(rmds, referenceFiles)
 	}
-	
+
 	knitenv <- new.env()
 	bibs <- list.files(dir[1], '.bib$', ignore.case=TRUE)
 	if(length(bibs) > 0) {
@@ -79,12 +79,12 @@ buildRmd <- function(dir = getwd(), clean=FALSE, log.dir, log.ext='.txt', ...) {
 		}
 		oldwd <- setwd(dirname(j))
 		tryCatch({
-			knit(basename(j), sub('.Rmd$', '.md', basename(j), ignore.case=TRUE), 
+			knitr::knit(basename(j), sub('.Rmd$', '.md', basename(j), ignore.case=TRUE),
 				 envir=knitenv, ...)
 		}, finally={ setwd(oldwd) })
 		if(!missing(log.dir)) { sink() }
 	}
-	
+
 	rmdinfo <- finfo
 	last.run <- Sys.time()
 	last.R.version <- R.version
